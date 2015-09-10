959e4dd0f8a8209f9019b9edb682d463c9635258
liJIREN <George@liJIRENs-MacBook-Pro.local>
2015-09-09 19:38:18 -0700

    initial commit

---
+++ ColorPlatformer/Assets/_Scripts/Events/CheckPointEvent.cs
@@ -0,0 +1,14 @@
+﻿public class CheckPointEvent : GameEvent
+{
+    public bool isEntered;
+    public int CharacterID;
+    public int iCheckPointID;
+    public CheckPointEvent(bool isEntered, int CharacterID, int iCheckPointID)
+    {
+        this.isEntered = isEntered;
+        this.CharacterID = CharacterID;
+        this.iCheckPointID = iCheckPointID;
+    }
+
+
+}
